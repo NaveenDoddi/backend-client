738abaed32112fe9ebae59c5e77c37cbcbeb935e
naveendoddi <prasadnaveen847@gmail.com>
2025-01-31 17:58:19 +0530

    adding real server in this

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
-// const connectDB = require('./db');
 
 const app = express();
 
-// Connect to MongoDB
-// connectDB();
-
-// Middleware
+// Enable CORS
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-      res.send("Server deployed and running on vercel.");
+// Route to fetch data from Incredible India
+app.get('/api/inc-home', async (req, res) => {
+  try {
+    const response = await axios.get('https://www.incredibleindia.gov.in/en');
+    res.send(response.data);
+  } catch (error) {
+    console.error('Error fetching Incredible India page:', error);
+    res.status(500).send('Error fetching Incredible India page');
+  }
 });
-app.use(cors({
-      origin: 'http://localhost:9000'  //to be changed later to vercel url
-}));
 
-// You can add more route files here as your application grows
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/posts', require('./routes/posts'));
+app.get('/api/wiki', async (req, res) => {
+  const title = req.params.title
+  try {
+    const response = await axios.get(`https://www.wikipedia.com/${title}`);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching wikipedia page');
+  }
+});
 
-const port = process.env.PORT || 9000;
 
-app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
